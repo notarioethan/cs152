e986eafe222f30f038ca8cbfdc662ba773372c46
notarioethan <133898526+notarioethan@users.noreply.github.com>
2025-12-08 22:18:34 -0800

    q5

--- T2/q5.hs
+++ T2/q5.hs
@@ -1,14 +1,23 @@
--- a. ltrim: removes all items from the start of the list that satisfy the predicate function.
+main :: IO()
+main = do
+  print (ltrim odd myList)
+  print (rtrim odd myList)
+  print (trim odd myList)
+
+-- remove from front
 ltrim :: (a -> Bool) -> [a] -> [a]
 ltrim _ [] = []
 ltrim p (x:xs)
   | p x       = ltrim p xs
   | otherwise = x:xs
 
--- b. rtrim: removes all items from the end of the list that satisfy the predicate function.
+-- remove from end
 rtrim :: (a -> Bool) -> [a] -> [a]
 rtrim p xs = reverse (ltrim p (reverse xs))
 
--- c. trim: removes all items from both the start and end of the list that satisfy the predicate function.
+-- remove from both
 trim :: (a -> Bool) -> [a] -> [a]
 trim p xs = ltrim p (rtrim p xs)
+
+myList :: [Int]
+myList = [1, 2, 3]
